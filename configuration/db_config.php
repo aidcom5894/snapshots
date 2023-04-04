9da5937050bf59e3be911cc7c85577bf645b0b2b
aidcom5894 <vivekrobinkujur_jsr@hotmail.com>
2023-04-04 07:49:39 +0530

    Updated

---
+++ configuration/db_config.php
@@ -0,0 +1,19 @@
+<?php 
+
+$hostname = 'localhost';
+$username = 'root';
+$password = 'Admin1234#@';
+$dbname = 'snapshots';
+
+$config = mysqli_connect($hostname,$username,$password,$dbname);
+
+if(!$config)
+{
+	echo "Snapshot Connection to Database Failed with Error:" . mysqli_connect_error();
+}
+else
+{
+	echo "Snapshot Successfully Connected to Database";
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,177 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="keywords" content="Digital marketing agency, Digital marketing company, Digital marketing services">
+    <meta name="description" content="Jano creative multipurpose is a beautiful website template designed for SEO & Digital Agency websites.">
+        <meta property="og:site_name" content="Jano">
+        <meta property="og:url" content="https://heloshape.com/">
+        <meta property="og:type" content="website">
+        <meta property="og:title" content="Jano - Creative Multipurpose Bootstrap 5 Template">
+    <meta name='og:image' content='images/assets/ogg.png'>
+    <!-- For IE -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- For Resposive Device -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- For Window Tab Color -->
+    <!-- Chrome, Firefox OS and Opera -->
+    <meta name="theme-color" content="#1d2b40">
+    <!-- Windows Phone -->
+    <meta name="msapplication-navbutton-color" content="#1d2b40">
+    <!-- iOS Safari -->
+    <meta name="apple-mobile-web-app-status-bar-style" content="#1d2b40">
+    <title>Jano - Creative Multipurpose Bootstrap 5 Template</title>
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" sizes="56x56" href="images/fav-icon/icon.png">
+    <!-- Main style sheet -->
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="all">
+    <!-- responsive style sheet -->
+    <link rel="stylesheet" type="text/css" href="css/responsive.css" media="all">
+
+    <!-- Fix Internet Explorer ______________________________________-->
+    <!--[if lt IE 9]>
+      <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+      <script src="vendor/html5shiv.js"></script>
+      <script src="vendor/respond.js"></script>
+    <![endif]-->  
+  </head>
+
+  <body>
+
+    <div class="main-page-wrapper">
+      <!-- 
+      =============================================
+        Feature Section Fifty One
+      ============================================== 
+      -->
+      <div class="fancy-feature-fiftyOne position-relative mt-200">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-7 wow fadeInLeft">
+              <div class="title-style-five mb-65 lg-mb-40">
+                <div class="sc-title-two d-inline-block fst-italic position-relative">Our Recent Work</div>
+                <h2 class="main-title fw-500 tx-dark">Check out our Portfolio.</h2>
+              </div>
+            </div>
+          </div>
+        </div> <!-- /.container -->
+        <img src="images/lazy.svg" data-src="images/shape/shape_172.svg" alt="" class="lazy-img shapes shape-two">
+        <img src="images/lazy.svg" data-src="images/shape/shape_175.svg" alt="" class="lazy-img shapes shape-three">
+      </div> <!-- /.fancy-feature-fiftyOne -->
+
+
+
+      <!-- 
+      =============================================
+        Portfolio Gallery Nine
+      ============================================== 
+      -->
+      <div class="portfolio-gallery-nine pt-30">
+        <div class="container">
+          <ul class="style-none text-center isotop-menu-wrapper g-control-nav-three">
+            <li class="is-checked" data-filter="*">All</li>
+            <li data-filter=".marketing">Marketing</li>
+            <li data-filter=".application">Application</li>
+            <li data-filter=".design">Design</li>
+            <li data-filter=".dev">Development</li>
+          </ul>
+
+          <div id="isotop-gallery-wrapper" class="grid-2column pt-90 lg-pt-50">
+            <div class="grid-sizer"></div>
+            <div class="isotop-item application dev">
+              <div class="portfolio-block-seven mb-70 lg-mb-40">
+                <div class="img-meta"><img src="images/gallery/img_55.jpg" alt="" class="w-100 tran5s"></div>
+                <div class="caption pt-20">
+                  <span class="tag fst-italic position-relative">Illustration</span>
+                  <h6><a href="portfolio-details-v2.html" class="pj-title">Retro & Bold Art.</a></h6>
+                </div> <!-- /.caption -->
+              </div> <!-- /.portfolio-block-seven -->
+            </div> <!-- /.item -->
+            <div class="isotop-item design marketing">
+              <div class="portfolio-block-seven mb-70 lg-mb-40">
+                <div class="img-meta"><img src="images/gallery/img_56.jpg" alt="" class="w-100 tran5s"></div>
+                <div class="caption pt-20">
+                  <span class="tag fst-italic position-relative">Application</span>
+                  <h6><a href="portfolio-details-v2.html" class="pj-title">Hotel Managment App.</a></h6>
+                </div> <!-- /.caption -->
+              </div> <!-- /.portfolio-block-seven -->
+            </div> <!-- /.item -->
+            <div class="isotop-item application design marketing">
+              <div class="portfolio-block-seven mb-70 lg-mb-40">
+                <div class="img-meta"><img src="images/gallery/img_57.jpg" alt="" class="w-100 tran5s"></div>
+                <div class="caption pt-20">
+                  <span class="tag fst-italic position-relative">Development</span>
+                  <h6><a href="portfolio-details-v2.html" class="pj-title">Creative Agency Dev.</a></h6>
+                </div> <!-- /.caption -->
+              </div> <!-- /.portfolio-block-seven -->
+            </div> <!-- /.item -->
+            <div class="isotop-item application dev">
+              <div class="portfolio-block-seven mb-70 lg-mb-40">
+                <div class="img-meta"><img src="images/gallery/img_58.jpg" alt="" class="w-100 tran5s"></div>
+                <div class="caption pt-20">
+                  <span class="tag fst-italic position-relative">Design</span>
+                  <h6><a href="portfolio-details-v2.html" class="pj-title">Retro & Bold Art.</a></h6>
+                </div> <!-- /.caption -->
+              </div> <!-- /.portfolio-block-seven -->
+            </div> <!-- /.item -->
+            <div class="isotop-item design marketing">
+              <div class="portfolio-block-seven mb-70 lg-mb-40">
+                <div class="img-meta"><img src="images/gallery/img_59.jpg" alt="" class="w-100 tran5s"></div>
+                <div class="caption pt-20">
+                  <span class="tag fst-italic position-relative">Mockup</span>
+                  <h6><a href="portfolio-details-v2.html" class="pj-title">Bold & Dark Mocup.</a></h6>
+                </div> <!-- /.caption -->
+              </div> <!-- /.portfolio-block-seven -->
+            </div> <!-- /.item -->
+            <div class="isotop-item design marketing">
+              <div class="portfolio-block-seven mb-70 lg-mb-40">
+                <div class="img-meta"><img src="images/gallery/img_60.jpg" alt="" class="w-100 tran5s"></div>
+                <div class="caption pt-20">
+                  <span class="tag fst-italic position-relative">Branding</span>
+                  <h6><a href="portfolio-details-v2.html" class="pj-title">Hotel Managment App.</a></h6>
+                </div> <!-- /.caption -->
+              </div> <!-- /.portfolio-block-seven -->
+            </div> <!-- /.item -->
+          </div>
+        </div> <!-- /.container -->
+      </div> <!-- /.portfolio-gallery-nine -->
+
+
+
+
+      <button class="scroll-top">
+        <i class="bi bi-arrow-up-short"></i>
+      </button>
+      
+   
+
+
+
+
+      <!-- jQuery first, then Bootstrap JS -->
+      <!-- jQuery -->
+    <script src="vendor/jquery.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- WOW js -->
+    <script src="vendor/wow/wow.min.js"></script>
+    <!-- Slick Slider -->
+    <script src="vendor/slick/slick.min.js"></script>
+    <!-- Fancybox -->
+    <script src="vendor/fancybox/dist/jquery.fancybox.min.js"></script>
+    <!-- Lazy -->
+      <script src="vendor/jquery.lazy.min.js"></script>
+      <!-- js Counter -->
+    <script src="vendor/jquery.counterup.min.js"></script>
+    <script src="vendor/jquery.waypoints.min.js"></script>
+    <!-- isotop -->
+    <script  src="vendor/isotope.pkgd.min.js"></script>
+    <!-- validator js -->
+      <script src="vendor/validator.js"></script>
+
+    <!-- Theme js -->
+    <script src="js/theme.js"></script>
+    </div> <!-- /.main-page-wrapper -->
+  </body>
+</html>
